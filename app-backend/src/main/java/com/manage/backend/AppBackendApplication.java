86333c648354965bf981701a4f8551c4b24a9b52
kexiangbai （白轲祥） <kexiangbai@maxxipoint.com>
2019-09-25 12:27:19 +0800

    提交backend服务注册

---
+++ app-backend/src/main/java/com/manage/backend/AppBackendApplication.java
@@ -0,0 +1,15 @@
+package com.manage.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBackendApplication.class, args);
+	}
+
+}
