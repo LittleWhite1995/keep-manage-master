0bc07d10ea3b05fc839e776da2345cf2d1452d48
kexiangbai （白轲祥） <kexiangbai@maxxipoint.com>
2019-09-25 12:16:33 +0800

    提交初始化项目

---
+++ src/main/java/com/keep/manage/master/KeepManageMasterApplication.java
@@ -0,0 +1,13 @@
+package com.keep.manage.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepManageMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepManageMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keep/manage/master/KeepManageMasterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.keep.manage.master;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KeepManageMasterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
